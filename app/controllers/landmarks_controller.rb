336232b7e0b3591b24253c705d9acad7cc11b052
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-16 20:32:06 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -22,7 +22,7 @@ class LandmarksController < ApplicationController
   post '/landmarks' do
     @landmark = Landmark.new(params["landmark"])
     @landmark.save
-    redirect to "/figures/#{@landmark.id}"
+    redirect to "/landmarkss/#{@landmark.id}"
   end
 
 end
