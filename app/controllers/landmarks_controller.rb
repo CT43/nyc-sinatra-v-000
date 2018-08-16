b8305c12ac990034abf52e541c984f09bf3d60a7
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-16 20:19:01 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,48 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by(id: params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @figure = Landmark.find_by(id: params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  post '/landmarks' do
+    @figure = Landmark.new(params["figure"])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    @figure.save
+    redirect to "/landmarks/#{@figure.id}"
+  end
+
+  patch '/landmarks/:id' do
+    @figure = Landmark.find_by(id: params[:id])
+      @figure.update(params[:figure])
+      if !params[:landmark][:name].empty?
+        @figure.landmarks << Landmark.create(params[:landmark])
+      end
+
+      if !params[:title][:name].empty?
+        @figure.titles << Title.create(params[:title])
+      end
+
+      @figure.save
+      redirect to "/landmarks/#{@figure.id}"
+  end
 end
