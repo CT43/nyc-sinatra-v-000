7fa711a038a87e8b41a43d6768cd17aadf310b39
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-16 19:46:45 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -7,7 +7,9 @@ class FiguresController < ApplicationController
   post '/figures' do
     @figure = Figure.new(name: params[:name])
     @figure.title = Title.find_or_create_by(name: params[:name])
-    @figure.title_ids = params[title_ids]
+    @figure.title_ids = params[:title_ids]
+    @figure.landmark = Title.find_or_create_by(name: params[:name])
+    @figure.landmark_ids = params[:landmark_ids]
     @figure.save
 
 end
